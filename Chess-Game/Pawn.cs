e40e6e1d028b8c2324844d3cf01dbde39c082024
unknown <18120186@student.hcmus.edu.vn>
2021-11-04 22:32:45 +0700

    UI and ChessUnit class

---
+++ Chess-Game/Pawn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Game
+{
+    class Pawn: Unit
+    {
+        Dictionary<int, int> PossibleMove = new Dictionary<int, int>();
+
+        Pawn()
+        {
+            //List<int> myPosition = getPosition();
+            //PossibleMove.Add(myPosition[0], myPosition[1]);
+        }
+    }
+}
---
+++ Chess-Game/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Game
+{
+    class Unit
+    {
+        private int HorizontalPosition = new int();
+        private int VerticalPostition = new int();
+
+        public List<int> getPosition()
+        {
+            List<int> pos = new List<int>() { VerticalPostition, HorizontalPosition };
+            return pos;
+        }
+    }
+}
